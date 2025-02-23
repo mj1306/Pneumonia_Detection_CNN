7256335316253215c21affc0e092013fbebc9512
Mathew <m8joseph@uwaterloo.ca>
2025-02-24 01:35:27 +0400

    Tackling overfitting -Unfreezinf the last block to fine tune the model to our dataset -Added code to track the history of losses and accuracie -Network is still performing very poorly on validation (almost 0 improvement from inital loss), indicating signiicant overfitting

---
+++ Network_Architecture/dataset_loading.py
@@ -0,0 +1,37 @@
+#%%Imports
+import numpy as np
+import tensorflow as tf
+import keras as krs
+import matplotlib.pyplot as plt
+
+#%%
+BATCH_SIZE = 32
+IMG_SIZE = (160,160) #Fixed image size in-case dataset consists of images of different sizes
+train_directory = r"Fracture_Detection-1\datasets\Bone_Break_Classification\test"
+valid_directory = r"Fracture_Detection-1\datasets\Bone_Break_Classification\valid"
+
+#%%
+train_dataset = krs.preprocessing.image_dataset_from_directory(train_directory,
+                                             shuffle=True,
+                                             batch_size=BATCH_SIZE,
+                                             image_size=IMG_SIZE,
+                                             seed=42)
+validation_dataset = krs.preprocessing.image_dataset_from_directory(valid_directory,
+                                             shuffle=True,
+                                             batch_size=BATCH_SIZE,
+                                             image_size=IMG_SIZE,
+                                             seed=42)
+
+#%%Test to see labelled images
+class_names = train_dataset.class_names
+
+plt.figure(figsize=(10, 10))
+for images, labels in train_dataset.take(1):
+    for i in range(9):
+        ax = plt.subplot(3, 3, i + 1)
+        plt.imshow(images[i].numpy().astype("uint8"))
+        plt.title(class_names[labels[i]])
+        plt.axis("off")
+
+
+# %%
